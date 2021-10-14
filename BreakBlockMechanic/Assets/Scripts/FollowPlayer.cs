7a369398ed20da112ae1320ada4e0da72be3475c
Evan Prince <evanmprince@gmail.com>
2021-10-14 10:14:26 -0500

    Camera doesn't poke over the edge of the map. (It does, but only a little) :)

--- BreakBlockMechanic/Assets/Scripts/FollowPlayer.cs
+++ BreakBlockMechanic/Assets/Scripts/FollowPlayer.cs
@@ -8,7 +8,6 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] Transform target; // stores our target (the player)
-    private PlayerHealth playerHealth;
 
     // variable to control how smoothly the camera moves
     private float smoothSpeed = 2f;
@@ -17,20 +16,58 @@ public class FollowPlayer : MonoBehaviour
     Vector3 offset; // TODO, dynamically offset the camera from the player if it to close to edge of map;
 
     bool trackPlayer = false; // bool to let the camera know if it should follow the player
-    bool goToCenter = false; // bool to let the camera know if it should go to center
+    bool goToCenter = true; // bool to let the camera know if it should go to center
 
     [SerializeField] int maxCameraSize; // stores set the max camera size (map size)
     [SerializeField] int minCameraSize; // stores min camera size (15 unless we have a niche reason to change it)
 
+    [SerializeField] Vector3 mapCenter;
+    [SerializeField] float mapHeight;
+    [SerializeField] float mapWidth;
+
+    float mapTopLimit;
+    float mapBottomLimit;
+    float mapLeftLimit;
+    float mapRightLimit;
+
+    float cameraTopLimit;
+    float cameraBottomLimit;
+    float cameraLeftLimit;
+    float cameraRightLimit;
+
+    const float pixelRatio = 16f / 9f;
+
     private void Start()
     {
-        playerHealth = GameObject.Find("Ember").GetComponent<PlayerHealth>();
+        calculateMapLimits(); // calculate the edges of the map
+    }
 
+    // calculates the x and y values of the edges of the map based on the map's center position and the map size
+    // map size is stored in [SerializeField] variables which need to be changed for each map
+    private void calculateMapLimits()
+    {
+        mapTopLimit = mapCenter.y + mapHeight / 2;
+        mapBottomLimit = mapCenter.y - mapHeight / 2;
+        mapLeftLimit = mapCenter.x - mapWidth / 2;
+        mapRightLimit = mapCenter.x + mapWidth / 2;
+    }
+
+    // calculates the x and y values of the edges of the camera based on the cameras position and orthographic size
+    private void calculateCameraLimits()
+    {
+        // position equals camera's y position + the orthographic size (orthosize = 1/2 of the camera height in worldspace units)
+        cameraTopLimit = transform.position.y + (GetComponent<Camera>().orthographicSize);
+        cameraBottomLimit = transform.position.y - (GetComponent<Camera>().orthographicSize);
+        // same as above, but for width we need to multiply scale by the pixel ratio (16/9)
+        cameraLeftLimit = transform.position.x - (GetComponent<Camera>().orthographicSize * pixelRatio);
+        cameraRightLimit = transform.position.x + (GetComponent<Camera>().orthographicSize * pixelRatio);
     }
 
     // called each frame right after update
     private void FixedUpdate()
     {
+        calculateCameraLimits(); // find the edges of the camera
+        calculateOffset(); // find what the offset should be (so the camera doesn't look over the edge of the map)
         if (trackPlayer) // if the camera should be following the player
         {
             desiredPosition = target.position + offset; // store targets position so we can edit z value (offset variable unused so far)
@@ -41,7 +78,7 @@ public class FollowPlayer : MonoBehaviour
         }
         else
         {
-            goToLocation(playerHealth.getMapCenter(), 1);
+            goToLocation(mapCenter, 1);
             GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, 26, 1 * Time.deltaTime);
         }
         
@@ -71,5 +108,36 @@ public class FollowPlayer : MonoBehaviour
 
         // sets the position of the camera to the smoothed position
         transform.position = smoothedPosition;
+    }
+
+    // calculates the offset of the camera so that it doesn't look beyond the edge of the map
+    private void calculateOffset()
+    {
+        if (cameraTopLimit > mapTopLimit) // if the top of the camera passes the top of the map
+        {
+            offset.y = (mapTopLimit - cameraTopLimit) * 9; // set the offset in the y = to the top of the maps y value - the top of the cameras y value;
+            // multiply by nine because the camera is still trying to go the player, so it needs some extra offset to overpower its base tracking
+        }
+        else if (cameraBottomLimit < mapBottomLimit)
+        {
+            offset.y = (mapBottomLimit - cameraBottomLimit) * 9;
+        }
+        else
+        {
+            offset.y = 0;
+        }
+
+        if (cameraRightLimit > mapRightLimit)
+        {
+            offset.x = (mapRightLimit - cameraRightLimit) * 9;
+        }
+        else if (cameraLeftLimit < mapLeftLimit)
+        {
+            offset.x = (mapLeftLimit - cameraLeftLimit) * 9;
+        }
+        else
+        {
+            offset.x = 0;
+        }
     }
 }
