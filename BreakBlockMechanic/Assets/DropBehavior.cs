3f6fc43ab0d266506642413362718e0a57ea8bb9
EPrince99 <92382794+EPrince99@users.noreply.github.com>
2021-10-14 12:55:40 -0500

    Dripping Ice
    Added a dripping ice prefab with two stages. It drips slowly if 2 fires are lit, and quickly if 4 are lit. These numbers can be changed in unity under the dripping behavior script on the DrippingIce prefab

---
+++ BreakBlockMechanic/Assets/DropBehavior.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropBehavior : MonoBehaviour
+{
+    PlayerHealth playerHealth;
+    private float timePassed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = GameObject.Find("Ember").GetComponent<PlayerHealth>();
+        timePassed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+        // This destroys the object after 10 seconds, just in case
+        if(timePassed > 10)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // if the object collides with the player
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            // kill the player
+            playerHealth.deathEffect();
+        }
+        // if the drop hits anything, destroy itself.
+        Destroy(this.gameObject);
+    }
+}
--- BreakBlockMechanic/Assets/Scripts/DrippingBehavior.cs
+++ BreakBlockMechanic/Assets/Scripts/DrippingBehavior.cs
@@ -6,12 +6,18 @@ public class DrippingBehavior : MonoBehaviour
 {
 
     CampfireTracker campfireTracker;
-    [SerializeField] int firesRequired;
+    [SerializeField] int firesRequiredStg1;
+    [SerializeField] int firesRequiredStg2;
+
     float timePassed;
-    [SerializeField] float dripFrequency;
+    [SerializeField] float dripFrequencyStg1;
+    [SerializeField] float dripFrequencyStg2;
+    [SerializeField] GameObject prefab;
+
     // Start is called before the first frame update
     void Start()
     {
+        // will be used to get the number of fires lit in the level
         campfireTracker = GameObject.Find("ScriptHolder").GetComponent<CampfireTracker>();
         timePassed = 0;
     }
@@ -19,13 +25,23 @@ public class DrippingBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(campfireTracker.getNumfiresLit() >= firesRequired)
+        // if the number of fires lit is at least the value of stg1 and under the value of stg2
+        if(campfireTracker.getNumfiresLit() >= firesRequiredStg1 && campfireTracker.getNumfiresLit() < firesRequiredStg2)
+        {
+            timePassed += Time.deltaTime; // track time
+            if(timePassed > dripFrequencyStg1) // if enough time has passed
+            {
+                timePassed = 0; // reset timer
+                Instantiate(prefab, transform.position, transform.rotation); // create water drop
+            }
+        }
+        else if (campfireTracker.getNumfiresLit() >= firesRequiredStg2) // same thing but for stage 2
         {
             timePassed += Time.deltaTime;
-            if(timePassed > dripFrequency)
+            if (timePassed > dripFrequencyStg2)
             {
                 timePassed = 0;
-                Debug.Log("Drip Drip Drip Bitch");
+                Instantiate(prefab, transform.position, transform.rotation);
             }
         }
         
