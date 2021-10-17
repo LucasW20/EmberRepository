1c73a814d86e35a3842e2ef1a361eeb3a64a990d
Awilliams100 <williamsa6752@my.uwstout.edu>
2021-10-17 15:44:43 -0500

    WorkingAndAnimatedTumbleweed
    The tumbleweed is now updated to have the tumbleweed art and now rotates/is animated.

--- BreakBlockMechanic/Assets/Scripts/TumbleweedMovement.cs
+++ BreakBlockMechanic/Assets/Scripts/TumbleweedMovement.cs
@@ -6,7 +6,8 @@ public class TumbleweedMovement : MonoBehaviour
 {
     Rigidbody2D detector;
     GameObject ember;
-    CircleCollider2D tumblelider;
+    PolygonCollider2D tumblelider;
+    //Force for the tumbleweed flies to the left
     Vector2 force = new Vector2(-75, 0);
 
     // Start is called before the first frame update
@@ -18,8 +19,8 @@ public class TumbleweedMovement : MonoBehaviour
         //Finds the ember game object
         ember = GameObject.Find("Ember");
 
-        //
-        tumblelider = GetComponent<CircleCollider2D>();
+        //Grabs the Polygon collider of the tumbleweed
+        tumblelider = GetComponent<PolygonCollider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -27,37 +28,43 @@ public class TumbleweedMovement : MonoBehaviour
         //If the object that has gone into the trigger is the ember       
         if (collision.gameObject.name.Equals("Ember"))
         {
-            //The ice is kinematic to so it stays on the ceiling. 
-            //Turn that off so it can fall
+            //The tumbleweed is kinematic to so it doesn't move/fall 
+            //Turn that off so it can move to the left
             detector.isKinematic = false;
             detector.drag = 0;
             detector.angularDrag = 0;
             detector.AddForce(force, ForceMode2D.Impulse);
             //Adds the force declared above so it falls fast
 
+            //Starts the coroutine that is declared below
             StartCoroutine(IgnoreWallCoroutine());
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //If the object that collides with the object is the ember, it destroys the ice
+        //If the object that collides with the object is the ember, it destroys the tumbleweed
         if (collision.gameObject.name.Equals("Ember"))
         {
             ember.GetComponent<PlayerHealth>().deathEffect();
             Destroy(gameObject);
         }
+       //If it doesn't hit the ember, it will delete after it hits a wall
         else if (collision.gameObject.CompareTag("Wall"))
         {
             Destroy(gameObject);
         }
     }
 
+    //This is the coroutine that is called in the OnTriggerEnter
+    //This will, as the name suggest, make the tumbleweed ignore the first collision with the wall
     public IEnumerator IgnoreWallCoroutine()
     {
-        //yield on a new YieldInstruction that waits for 1 second.
-        yield return new WaitForSeconds(0.12f);
+        //yield on a new YieldInstruction that waits for 0.14 seconds before executing what is below
+        yield return new WaitForSeconds(0.14f);
 
+        //Collider is disabled to start so it doesn't delete itself when it flies through the wall. 
+        //It is enabled once the 0.14 seconds pass
         tumblelider.enabled = true;
     }
 }
