a723c772ae43c342ec299b3a8d772751afa561cd
EPrince99 <92382794+EPrince99@users.noreply.github.com>
2021-10-18 17:25:14 -0500

    Better Health Bar

--- BreakBlockMechanic/Assets/Scripts/HealthBar.cs
+++ BreakBlockMechanic/Assets/Scripts/HealthBar.cs
@@ -6,15 +6,21 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
 
     public void setHealth(float nHealth)
     {
         slider.value = nHealth;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+
     }
 
     public void setMaxHealth(float nMaxHealth)
     {
         slider.maxValue = nMaxHealth;
         slider.value = nMaxHealth;
+
+        fill.color = gradient.Evaluate(1f);
     }
 }
