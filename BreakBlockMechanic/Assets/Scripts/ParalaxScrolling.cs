05bf7e2cf6f141c228f6058c8e177e53adf80286
LucasW20 <lucas.wright552@gmail.com>
2021-10-19 08:33:24 -0500

    Y Parallax

--- BreakBlockMechanic/Assets/Scripts/ParalaxScrolling.cs
+++ BreakBlockMechanic/Assets/Scripts/ParalaxScrolling.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class ParalaxScrolling : MonoBehaviour {
 
     private float lastX;
+    private float lastY;
     public float paralaxSpeed;
-
-
     
     // Start is called before the first frame update
     void Start() {
         lastX = Camera.main.transform.position.x;
+        lastY = Camera.main.transform.position.y;
     }
 
     // Update is called once per frame
     void Update() {
         float deltaX = Camera.main.transform.position.x - lastX;
+        float deltaY = Camera.main.transform.position.y - lastY;
 
         transform.position += Vector3.right * (deltaX * paralaxSpeed);
+        transform.position += Vector3.up * (deltaY * paralaxSpeed);
+
 
         lastX = Camera.main.transform.position.x;
+        lastY = Camera.main.transform.position.y;
         
     }
 }
