e721cce9a3732f8a98e42943c3fd6d3a9e6616f3
LucasW20 <lucas.wright552@gmail.com>
2021-11-29 22:55:49 -0600

    Credits Level Built

--- BreakBlockMechanic/Assets/Scripts/CreditsScript.cs
+++ BreakBlockMechanic/Assets/Scripts/CreditsScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /***
  * Handles the unique behaviour of the credits level.
@@ -10,14 +11,49 @@ using UnityEngine;
  */
 public class CreditsScript : MonoBehaviour {
     private PlayerHealth ember;
+    private AudioSource bgmTrack;
+    private Image fadeImage;
 
     // Start is called before the first frame update
     void Start() {
+        fadeImage = GameObject.Find("FadeImage").GetComponent<Image>();
+        bgmTrack = GameObject.Find("SaveObject").GetComponent<AudioSource>();
         ember = GameObject.Find("Ember").GetComponent<PlayerHealth>();
+        ember.FreezeHealth();
     }
 
     // Update is called once per frame
     void Update() {
-        ember.resetTime();
+        //when the spacebar is pressed start the closing coroutine that exits the game
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            StartCoroutine(CloseCreditsCoroutine());
+        }
+    }
+
+    private IEnumerator CloseCreditsCoroutine() {
+        float waitTime = 5;
+        float fadeTime = 2.5f;
+        float time = 0;
+        float volStart = bgmTrack.volume;
+
+        //Wait for the zoom out to complete
+        yield return new WaitForSeconds(waitTime);
+
+        //fade out the image and the music
+        while (time < fadeTime) {
+            //update the time
+            time += Time.unscaledDeltaTime;
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, Mathf.Lerp(0f, 1f, time / fadeTime));
+            bgmTrack.volume = Mathf.Lerp(volStart, 0f, time / fadeTime);
+
+
+            yield return null; //finish the loop
+        }
+
+        //wait for a few more seconds for the fade to take place
+        yield return new WaitForSeconds(3);
+        
+        //change the scene to the main menu
+        SceneManager.LoadScene(0);
     }
 }
