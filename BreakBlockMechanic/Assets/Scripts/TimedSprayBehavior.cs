dbe7964ef596eb79d0f0d76243dd5532519aa7b1
Evan Prince <evanmprince@gmail.com>
2021-12-02 10:18:02 -0600

    TimedSpray Code Is now Unjanked

--- BreakBlockMechanic/Assets/Scripts/TimedSprayBehavior.cs
+++ BreakBlockMechanic/Assets/Scripts/TimedSprayBehavior.cs
@@ -7,7 +7,6 @@ public class TimedSprayBehavior : MonoBehaviour
     Collider2D playerCollider;
     BoxCollider2D boxCol;
     AreaEffector2D areaAffector;
-    [SerializeField] string direction; // the direction of the spray, accepts values of "up", "down", "left", and "right". case sensitive
     [SerializeField] float sprayHangTime; // how long the full hitbox of the spray remains
     [SerializeField] float timeBetweenSprays; // how long the wait is between sprays
 
@@ -23,7 +22,8 @@ public class TimedSprayBehavior : MonoBehaviour
         playerCollider = GameObject.Find("Ember").GetComponent<Collider2D>();
         areaAffector = GetComponent<AreaEffector2D>();
         boxCol = GetComponent<BoxCollider2D>();
-        setDirection(); // update the direction of the spray (based on direction variable)
+
+        areaAffector.forceAngle = 90;
     }
 
     // Update is called once per frame
@@ -54,58 +54,12 @@ public class TimedSprayBehavior : MonoBehaviour
         spraying = true; // start spraying again
     }
 
-    // function that sets the angle the spray shoots the player
-    private void setDirection()
-    {
-        switch (direction)
-        {
-            case "up":
-                areaAffector.forceAngle = 90;
-                break;
-            case "down":
-                areaAffector.forceAngle = 270;
-                break;
-            case "right":
-                areaAffector.forceAngle = 0;
-                break;
-            case "left":
-                areaAffector.forceAngle = 180;
-                break;
-            default:
-                break;
-
-        }
-    }
-
     // function that updates the size of the box collider of the spray
     private void spray(float nSprayWidth, float nSprayLength)
     {
-        switch (direction)
-        {
-            case "up":
                 boxCol.size = new Vector2(nSprayWidth, nSprayLength);
                 boxCol.offset = new Vector2(0, nSprayLength / 2);
-                break;
-            case "down":
-                boxCol.size = new Vector2(nSprayWidth, nSprayLength);
-                boxCol.offset = new Vector2(0, -nSprayLength / 2);
-                break;
-            case "right":
-                boxCol.size = new Vector2(nSprayLength, nSprayWidth);
-                boxCol.offset = new Vector2(nSprayLength / 2, 0);
-                break;
-            case "left":
-                boxCol.size = new Vector2(nSprayLength, nSprayWidth);
-                boxCol.offset = new Vector2(-nSprayLength / 2, 0);
-                break;
-            default:
-                break;
-        }
     }
 
-    public string getDirection()
-    {
-        return direction;
-    }
 
 }
